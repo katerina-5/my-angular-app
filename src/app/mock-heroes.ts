3fd722a4c2b669fb06207fe83a6a6d704bc4ca12
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-15 16:08:01 +0200

    Add mock-heroes.ts

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'N' },
+  { id: 13, name: 'B' },
+  { id: 14, name: 'C' },
+  { id: 15, name: 'M' },
+  { id: 16, name: 'Just Hero' },
+  { id: 17, name: 'D' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
