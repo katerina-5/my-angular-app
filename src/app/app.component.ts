1b27109e1de93573be4e424c5d3b22f535f0b755
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-15 16:05:29 +0200

    Add changes to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from '../app/hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+  
+  // selector— the component's CSS element selector
+  // templateUrl— the location of the component's template file.
+  // styleUrls— the location of the component's private CSS styles.
+
 })
+
 export class AppComponent {
   title = 'My First Angular App!';
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
