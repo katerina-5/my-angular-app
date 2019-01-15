905299f7b55602b16942e218ebe597be41ec0762
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-15 16:47:31 +0200

    Add changes to component heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
@@ -13,7 +14,7 @@ export class HeroesComponent implements OnInit {
   //   name: 'Ms. Fantastic'
   // };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   
@@ -21,9 +22,14 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
